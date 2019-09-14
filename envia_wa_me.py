b2ae9c307c33a36aa344cca427d85a7923d1ea73
maxiviento <leonmaxi@gmail.com>
2019-09-14 18:43:03 -0300

    Add files via upload

--- envia_wa_me.py
+++ envia_wa_me.py
@@ -13,6 +13,11 @@ import openpyxl as excel
 driver = webdriver.Chrome('chromedriver.exe')
 driver.get('https://web.whatsapp.com/')
 input("apreta algun boton luego de escanear el QR")
-driver.execute_script("window.open('https://api.whatsapp.com/send?phone=5493513883708&text=Como%20estas%20inscripta%20en%20el%20Programa%20bajo%20la%20modalidad%20CTI%2c%20deb%c3%a9s%20presentar%20el%20alta%20temprana%20de%20AFIP%20lo%20antes%20posible.%20Ten%c3%a9%20en%20cuenta%20que%20de%20acuerdo%20a%20la%20fecha%20en%20que%20presentes%20el%20documento%20de%20AFIP%2c%20se%20empezar%c3%a1%20a%20gestionar%20el%20tr%c3%a1mite%20de%20pago.%20%c2%a1Saludos%21&source=&data=');")
 
+driver.execute_script("window.location.href = 'https://web.whatsapp.com/send?phone=5493513883708&text=Cjhghj';")
 
+no_encuentra = driver.find_elements_by_xpath("//*[contains(text(), 'El número de teléfono compartido a través de la dirección URL es inválido')]")
+  
+#//*[@id="app"]/div/span[2]/div/span/div/div/div/div/div/div[2]/div
+
+#//*[@id="app"]/div/span[2]/div/span/div/div/div/div/div/div[1]
